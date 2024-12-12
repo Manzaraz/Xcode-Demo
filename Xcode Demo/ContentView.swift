e0d7143e4eaf2a7a7728e7a4db0dfde360828b5c
Christian Manzaraz <magnusmanz@icloud.com>
2024-12-12 11:58:02 -0300

    Initial Commit

---
+++ Xcode Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xcode Demo
+//
+//  Created by Christian Manzaraz on 12/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xcode Demo/Xcode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoApp.swift
+//  Xcode Demo
+//
+//  Created by Christian Manzaraz on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
